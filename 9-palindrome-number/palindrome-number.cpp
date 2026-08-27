45f39ecae8b103e4700c63872250463fb894210e
Abhilash_33 <abhilash.t2008@gmail.com>
2026-08-27 10:47:25 +0530

    Fix formatting of while loop in palindrome check

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -7,7 +7,7 @@ public:
         long long original = x;
         long long reverse = 0;
 
-        while (x > 0) {
+        while (x > 0)
             int digit = x % 10;
             reverse = reverse * 10 + digit;
             x /= 10;
